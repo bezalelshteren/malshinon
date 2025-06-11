75bf986ae3467a599d0eb17b4748b20d1249d541
bezalelshteren <a0556722346@gmail.com>
2025-06-11 10:55:11 +0300

    an error in the sql common

--- reporter.cs
+++ reporter.cs
@@ -4,37 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace malshinon
-{
-    public enum type
-    {
-        reporter,
-        target,
-        both,
-        potentialAgent
-    }
+//namespace malshinon
+//{
+//    public enum type
+//    {
+//        reporter,
+//        target,
+//        both,
+//        potentialAgent
+//    }
 
-    public class person
-    {
-        public int id { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string secretCode { get; set; }
-        public type type { get; set; }
-        public int numReports { get; set; }
-        public int numMentions { get; set; }
+//    public class person
+//    {
+//        public int id { get; set; }
+//        public string firstName { get; set; }
+//        public string lastName { get; set; }
+//        public string secretCode { get; set; }
+//        public type type { get; set; }
+//        public int numReports { get; set; }
+//        public int numMentions { get; set; }
 
-        public reporter() { }
+//        public reporter() { }
 
-        public reporter(string firstName, string lastName, string secretCode, type type, int numReports, int numMentions)
-        {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.secretCode = secretCode;
-            this.type = type;
-            this.numReports = numReports;
-            this.numMentions = numMentions;
+//        public reporter(string firstName, string lastName, string secretCode, type type, int numReports, int numMentions)
+//        {
+//            this.firstName = firstName;
+//            this.lastName = lastName;
+//            this.secretCode = secretCode;
+//            this.type = type;
+//            this.numReports = numReports;
+//            this.numMentions = numMentions;
 
-        }
-    }
-}
+//        }
+//    }
+//}
